4fceda1b1ec1b54a05e99f5c8bd30684652c34e8
Ariel Lothlorien <git@arielkauai.com>
2017-11-23 15:29:17 -0600

    Testing setup

---
+++ process/js/render.js
@@ -0,0 +1,7 @@
+var $ = jQuery = require('jquery');
+var bootstrap = require('bootstrap');
+
+//Test Bootstrap
+$(function() {
+	$("#mainWindow").append('<h3 class="text-success">App Loaded</h3>');
+})
